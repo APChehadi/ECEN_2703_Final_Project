095b233863e1b7599f95ca75527d66584c00f674
APChehadi <adampchehadi@gmail.com>
2021-04-26 09:02:29 -0600

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,14 +1,12 @@
-p = 0x10000000f
-q = 0x10000003d
-e = 0x10001
-n = p*q
-d = linearCongruence(e, 1, (p-1)*(q-1))
+import sympy
+import math
+import random
 
-M = 0x4543454e32373033
-print('M = ', hex(M))
+print(sympy.isprime(5))
 
-C = mod_exponent(M, e, n)
-print('C = ', hex(C))
+# Bob Private and Public Key Generation
+p = sympy.randprime(1, 1000000)
+q = sympy.randprime(1, 1000000)
 
-P = mod_exponent(C, d, n)
-print('P = ', hex(P))
+print(p)
+print(q)
